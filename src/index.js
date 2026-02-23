3881cc9214be1c8ee5ab9a7f97d52eb6d6145f62
sanskardagade <sanskardagade07@gmail.com>
2026-02-23 10:21:15 +0530

    db added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./db/index.js";
-dotenv.config({
-    path: "./.env",
-})
 
-const port = process.env.PORT  || 3000;
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
 
 connectDB()
-    .then(() =>{
-        app.listen(port,()=> {
-            console.log(`Example app listening at port http://locatlost:${port}`);
-        });
-    })
-    .catch((err) =>{
-        console.error("Monog DB Connection Error",err);
-        process.exit(1);
-    })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
