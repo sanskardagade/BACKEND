269df621f9a7112fd7e51cd60983dcb21c5935ef
sanskardagade <sanskardagade07@gmail.com>
2026-02-23 14:19:07 +0530

    user model create

---
+++ src/models/user.models.js
@@ -0,0 +1,74 @@
+import mongoose, { Schema } from "mongoose";
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+    this.password = await brcypt.hash(this.password,10);
+    next;
+})
+
+export const User = mongoose.model("User", userSchema);
