228aa1974dedad8f7ed94a177537d940110dedc4
sanskardagade <sanskardagade07@gmail.com>
2026-02-28 13:46:39 +0530

    login done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,6 +5,7 @@ import { asyncHandler } from "../utils/async-handler.js";
 import {emailVerificationMailgenContent, forgotPasswordMailgenContent, sendEmail} from "../utils/mail.js"
 
 
+
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -78,7 +79,55 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 
-export {registerUser}
+const login = asyncHandler(async (req,res) => {
+  const{email,password,username} = req.body;
+
+  if(!email){
+    throw new ApiError(400,"Email is required")
+  }
+
+  const user = await User.findOne({email})
+
+  if(!user){
+    throw new ApiError(400,"User does not exists")
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password)
+
+  if(!isPasswordValid){
+    throw new ApiError(400,"Invalid credentials")
+  }
+
+
+  const {refreshToken,accessToken}= await generateAccessAndRefreshTokens(user._id)
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+
+  const options = {
+    httpOnly: true,
+    secure:true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("refreshToken",refreshToken,options)
+  .json(
+    new ApiResponse(
+      200,
+      {
+        user:loggedInUser,
+        accessToken,
+        refreshToken
+      },
+      "User logged in successfully"
+    )
+  )
+})
+export {registerUser,
+        login}
 
 
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser,login } from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegisterValidator,userLoginValidator } from "../validators/index.js"; 
 
 const router = Router()
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(),validate,login);
 
 export default router;
