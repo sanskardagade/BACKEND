d732aeff9da37d5f5ad9d2bc4af81835630592f3
sanskardagade <sanskardagade07@gmail.com>
2026-03-02 09:18:45 +0530

    models done

--- src/models/projectmember.models.js
+++ src/models/projectmember.models.js
@@ -1,22 +1,28 @@
-import mongoose, { mongo, Schema } from "mongoose";
-import {AvailableUserRole, UserRolesEnum} from "../utils/constants.js"
+import mongoose, { Schema } from "mongoose";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
 
-const projectMemberSchema = new Schema ({
-    user:{
-        type: Schema.Types.ObjectId,
-        ref:"User",
-        required:true
+const projectMemberSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    project:{
-        type:Schema.Types.ObjectId,
-        ref:"Project",
-        required:true
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
     },
-    role:{
-        type:String,
-        enum:AvailableUserRole,
-        default:UserRolesEnum.MEMBER
-    }
-})
+    role: {
+      type: String,
+      enum: AvailableUserRole,
+      default: UserRolesEnum.MEMBER,
+    },
+  },
+  { timestamps: true },
+);
 
-export const ProjectMember = mongoose.model("ProjectMember",projectMemberSchema)
+export const ProjectMember = mongoose.model(
+  "ProjectMember",
+  projectMemberSchema,
+);
